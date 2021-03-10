c1e52be3cfe5bf4e7885ef8f2c26eaabf54831e1
yansaan <ian.ian.wold76@gmail.com>
2021-03-10 18:05:57 +0700

    menambah table

--- assets/statusBot.js
+++ assets/statusBot.js
@@ -1,5 +1,6 @@
 let statusWeb = document.getElementById('status')
 let errorText = document.getElementById('error')
+let tableStat = document.getElementById('table')
 
 //statusWeb.innerHTML = `status`
 window.onload = async function() {
@@ -9,13 +10,34 @@ window.onload = async function() {
       statusWeb.innerHTML = `I'm just wake up`
     } else {
       statusWeb.innerHTML = `I'm on break`
+      return
     }
     return s.json()
   }).then(r => {
-    
+    let statusTable = ''
+    r.detail.forEach(d => {
+      statusTable += `<tr>
+      <td>${d.name}</td>
+      <td>${d.type}</td>
+      <td class="">${(d.status)?'✅':'❌'}</td>
+      </tr>`
+    })
+
+    tableStat.innerHTML = `<table class="table table-dark table-borderless">
+    <thead>
+    <tr>
+    <th scope="col">Name</th>
+    <th scope="col">Type</th>
+    <th scope="col">Status</th>
+    </tr>
+    </thead>
+    <tbody>
+    ${statusTable}
+    </tbody>
+    </table>`
   }).catch(e => {
     errorText.textContent = e
     statusWeb.innerHTML = `I'm have a poblem`
   })
-  
+
 }
