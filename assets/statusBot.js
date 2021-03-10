6163c1b50af0fe42570bb8789cda588bf94d3b49
yansaan <ian.ian.wold76@gmail.com>
2021-03-10 16:08:34 +0700

    status

--- assets/statusBot.js
+++ assets/statusBot.js
@@ -4,9 +4,18 @@ let errorText = document.getElementById('error')
 //statusWeb.innerHTML = `status`
 window.onload = async function() {
 
-  return await fetch('https://billy-bot-1.yansaan.repl.co').then(s => s.json()).then(r => {}).catch(e => {
-      errorText.textContent = e
-      statusWeb.innerHTML = `I'm have a poblem`
-    })
-
+  await fetch('https://billy-bot-1.yansaan.repl.co').then(s => {
+    if (s.ok) {
+      statusWeb.innerHTML = `I'm just wake up`
+    } else {
+      statusWeb.innerHTML = `I'm on break`
+    }
+    return s.json()
+  }).then(r => {
+    
+  }).catch(e => {
+    errorText.textContent = e
+    statusWeb.innerHTML = `I'm have a poblem`
+  })
+  
 }
