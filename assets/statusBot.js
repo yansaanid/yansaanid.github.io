e4d8030b1fed082671b1f8aec9c90b4e90ed0533
yansaan <ian.ian.wold76@gmail.com>
2021-03-11 18:45:13 +0700

    memperbaiki chart

--- assets/statusBot.js
+++ assets/statusBot.js
@@ -3,14 +3,13 @@ let errorText = document.getElementById('error')
 let tableStat = document.getElementById('table')
 let billyChart = document.getElementById('billyChart')
 
-const labels = []
-const data = []
-const borderColor = []
-
 window.onload = async function() {
 
   await fetch('https://v1.nocodeapi.com/yansaan/uptime/uyUyRwkEZeMkvirR?monitors=787418020').then(r => r.json()).then(async r => {
     const stat = r.monitors[0]
+    const labels = []
+    const data = []
+    const backgroundColor = []
 
     switch (stat.status) {
       case 2:
@@ -26,47 +25,64 @@ window.onload = async function() {
       default:
         statusWeb.innerHTML = `I'm have a poblem`
         break;
+    }
+
+    const uptime = stat.all_time_uptime_ratio
+    
+    labels.push(`uptime ${Math.round(uptime)}%`)
+    labels.push(`downtime ${100 - (Math.round(uptime))}%`)
+    
+    data.push(uptime) 
+    data.push(100 - uptime)
+    
+    backgroundColor.push('white')
+    backgroundColor.push('gray')
+    
+
+    let getChart = new Chart(billyChart, {
+      type: 'pie',
+      data: {
+        labels,
+        datasets: [{
+          //label: '# milisec',
+          data,
+          backgroundColor,
+          borderColor: 'white',
+          borderWidth: 1
+        }]
       }
 
-      stat.response_times.forEach(c => {
-        const dates = new Date(c.datetime * 1000)
-        labels.push(`${dates.toLocaleString("en-US", {
-          timeStyle: "short"
-        })}`)
+    })
+    
+    if (stat.status !== 2) return
 
-        data.push(c.value)
-        borderColor.push('rgba(255, 99, 132, 1)')
-      })
-
-      let getChart = new Chart(billyChart, {
-        type: 'line',
-        data: {
-          labels,
-          datasets: [{
-            label: '# milisec',
-            data,
-            backgroundColor:
-            'rgba(255, 99, 132, 0.2)',
-            borderColor,
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true,
-                suggestedMax: 5000
-              }
-            }]
-          }
-        }
-
-      })
-
+    await fetch('https://billy-bot-1.yansaan.repl.co').then(r => r.json()).then(r => {
+    let statusTable = ''
+    r.detail.forEach(d => {
+      statusTable += `<tr>
+      <td>${d.name}</td>
+      <td>${d.type}</td>
+      <td class="">${(d.status)?'✅':'❌'}</td>
+      </tr>`
     })
 
-    /*await fetch('https://billy-bot-1.yansaan.repl.co').then(s => {
+    tableStat.innerHTML = `<table class="table table-dark table-borderless">
+    <thead>
+    <tr>
+    <th scope="col">Name</th>
+    <th scope="col">Type</th>
+    <th scope="col">Status</th>
+    </tr>
+    </thead>
+    <tbody>
+    ${statusTable}
+    </tbody>
+    </table>`
+  })
+
+  })
+
+  /*await fetch('https://billy-bot-1.yansaan.repl.co').then(s => {
     if (s.ok) {
       statusWeb.innerHTML = `I'm just wake up`
     } else {
@@ -101,4 +117,4 @@ window.onload = async function() {
     statusWeb.innerHTML = `I'm have a poblem`
   })*/
 
-  }
+}
