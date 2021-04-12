08c223bf3774d5955736e42ca401d609c757329f
yansaan <ian.ian.wold76@gmail.com>
2021-04-13 06:36:33 +0700

    fix boot

--- assets/statusBot.js
+++ assets/statusBot.js
@@ -62,7 +62,7 @@ window.onload = async function() {
       statusTable += `<tr>
       <td>${d.name}</td>
       <td>${d.type}</td>
-      <td class="">${(d.status)?'✅':'❌'}</td>
+      <td class="">${(d.enable)?'✅':'❌'}</td>
       </tr>`
     })
 
@@ -81,40 +81,5 @@ window.onload = async function() {
   })
 
   })
-
-  /*await fetch('https://billy-bot-1.yansaan.repl.co').then(s => {
-    if (s.ok) {
-      statusWeb.innerHTML = `I'm just wake up`
-    } else {
-      statusWeb.innerHTML = `I'm on break`
-      return
-    }
-    return s.json()
-  }).then(r => {
-    let statusTable = ''
-    r.detail.forEach(d => {
-      statusTable += `<tr>
-      <td>${d.name}</td>
-      <td>${d.type}</td>
-      <td class="">${(d.status)?'✅':'❌'}</td>
-      </tr>`
-    })
-
-    tableStat.innerHTML = `<table class="table table-dark table-borderless">
-    <thead>
-    <tr>
-    <th scope="col">Name</th>
-    <th scope="col">Type</th>
-    <th scope="col">Status</th>
-    </tr>
-    </thead>
-    <tbody>
-    ${statusTable}
-    </tbody>
-    </table>`
-  }).catch(e => {
-    errorText.textContent = e
-    statusWeb.innerHTML = `I'm have a poblem`
-  })*/
 
 }
