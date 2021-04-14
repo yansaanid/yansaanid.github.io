8450ff559e023cf84c053eaa9cda3a2f2c513f85
yansaan <ian.ian.wold76@gmail.com>
2021-04-14 17:38:43 +0700

    change billy bot look

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,45 @@
-const navBar = document.getElementById("navbar")
-const menu = document.getElementById("navbarNavAltMarkup")
-const themes = document.getElementById("themes")
-const body = document.body
-let navbarOpen = false
+const navBar = document.getElementById("navbar");
+const menu = document.getElementById("navbarNavAltMarkup");
+const themes = document.getElementById("themes");
+const body = document.body;
+let navbarOpen = false;
 
-const bgWhite = '255, 255, 255'
-const bgDark = '33, 33, 33'
-let navBg
+const bgWhite = "255, 255, 255";
+const bgDark = "33, 33, 33";
+let navBg;
 
-let theme = localStorage.getItem('theme')
+let theme = localStorage.getItem("theme");
 if (!theme) {
-  localStorage.setItem('theme', 'default')
-   navBg = bgWhite
+  localStorage.setItem("theme", "default");
+  navBg = bgWhite;
 } else {
-  if (theme === 'darker')
-    body.classList.replace('default', 'darker')
-  navBg = theme === 'darker' ? bgDark : bgWhite
+  if (theme === "darker") body.classList.replace("default", "darker");
+  navBg = theme === "darker" ? bgDark : bgWhite;
 }
 
-scroll()
+scroll();
 
 themes.onclick = e => {
-  e.preventDefault()
-  if (body.classList.contains('darker')) {
-    body.classList.replace('darker', 'default')
-    localStorage.setItem('theme', 'default')
-    navBg = bgWhite
+  e.preventDefault();
+  if (body.classList.contains("darker")) {
+    body.classList.replace("darker", "default");
+    localStorage.setItem("theme", "default");
+    navBg = bgWhite;
   } else {
-    body.classList.replace('default', 'darker')
-    localStorage.setItem('theme', 'darker')
-    navBg = bgDark
+    body.classList.replace("default", "darker");
+    localStorage.setItem("theme", "darker");
+    navBg = bgDark;
   }
-  scroll()
-}
+  scroll();
+};
 
 window.onscroll = () => {
-  scroll()
-}
+  scroll();
+};
 
 function scroll() {
   let top = window.scrollY,
-    calc = top / 200
+    calc = top / 200;
   let n = parseFloat(
     calc
       .toString()
@@ -49,31 +48,31 @@ function scroll() {
         i === 1 ? (s = `${s.charAt(0)}${s.charAt(1)}${s.charAt(2)}`) : s
       )
       .join(".")
-  )
+  );
 
-if (!navbarOpen) {
-  if (calc >= 1) {
-    navBar.style.backgroundColor = `rgba(${navBg}, 1)`
-    navBar.style.boxShadow = `0 .125rem 1rem rgba(0, 0, 0, .15)`
-  } else if (calc <= 0) {
-    navBar.style.backgroundColor = `rgba(${navBg}, 0)`
-    navBar.style.boxShadow = `0 -1rem 1rem rgba(0, 0, 0, .15)`
+  if (!navbarOpen) {
+    if (calc >= 1) {
+      navBar.style.backgroundColor = `rgba(${navBg}, 1)`;
+      navBar.style.boxShadow = `0 .125rem 1rem rgba(0, 0, 0, .15)`;
+    } else if (calc <= 0) {
+      navBar.style.backgroundColor = `rgba(${navBg}, 0)`;
+      navBar.style.boxShadow = `0 -1rem 1rem rgba(0, 0, 0, .15)`;
+    } else {
+      navBar.style.backgroundColor = `rgba(${navBg}, ${calc})`;
+      navBar.style.boxShadow = `0 ${calc - 0.875}rem 1rem rgba(0, 0, 0, .15)`;
+    }
   } else {
-    navBar.style.backgroundColor = `rgba(${navBg}, ${calc})`
-    navBar.style.boxShadow = `0 ${calc - 0.875}rem 1rem rgba(0, 0, 0, .15)`
+    navBar.style.backgroundColor = `rgba(${navBg}, 1)`;
+    navBar.style.boxShadow = `0 .125rem 1rem rgba(0, 0, 0, .15)`;
   }
-} else {
-  navBar.style.backgroundColor = `rgba(${navBg}, 1)`
-  navBar.style.boxShadow = `0 .125rem 1rem rgba(0, 0, 0, .15)`
-}
 }
 
 menu.addEventListener("show.bs.collapse", function () {
-  navbarOpen = true
-  navBar.style.backgroundColor = `rgba(${navBg}, 1)`
-  navBar.style.boxShadow = `0 .125rem 1rem rgba(0, 0, 0, .15)`
-})
+  navbarOpen = true;
+  navBar.style.backgroundColor = `rgba(${navBg}, 1)`;
+  navBar.style.boxShadow = `0 .125rem 1rem rgba(0, 0, 0, .15)`;
+});
 menu.addEventListener("hidden.bs.collapse", function () {
-  navbarOpen = false
-  scroll()
-})
+  navbarOpen = false;
+  scroll();
+});
--- assets/statusBot.js
+++ assets/statusBot.js
@@ -1,72 +1,97 @@
-let statusWeb = document.getElementById('status')
-let errorText = document.getElementById('error')
-let tableStat = document.getElementById('table')
-let billyChart = document.getElementById('billyChart')
+let statusWeb = document.getElementById("status");
+let tableStat = document.getElementById("table");
+let billyChart = document.getElementById("billyChart");
+let cardDetail = document.getElementById("card-detail");
+const closeCard = document.getElementById("close-link");
 
-window.onload = async function() {
+window.onload = async function () {
+  await fetch(
+    "https://v1.nocodeapi.com/yansaan/uptime/uyUyRwkEZeMkvirR?monitors=787418020"
+  )
+    .then(r => r.json())
+    .then(async r => {
+      const stat = r.monitors[0];
+      const labels = [];
+      const data = [];
+      const backgroundColor = [];
 
-  await fetch('https://v1.nocodeapi.com/yansaan/uptime/uyUyRwkEZeMkvirR?monitors=787418020').then(r => r.json()).then(async r => {
-    const stat = r.monitors[0]
-    const labels = []
-    const data = []
-    const backgroundColor = []
+      switch (stat.status) {
+        case 2:
+          statusWeb.innerHTML = `I've been running something`;
+          break;
+        case 0:
+          statusWeb.innerHTML = `I'm on break`;
+          break;
+        case 1:
+          statusWeb.innerHTML = `I'm just wake up`;
+          break;
 
-    switch (stat.status) {
-      case 2:
-        statusWeb.innerHTML = `I've been running something`
-        break;
-      case 0:
-        statusWeb.innerHTML = `I'm on break`
-        break;
-      case 1:
-        statusWeb.innerHTML = `I'm just wake up`
-        break;
-
-      default:
-        statusWeb.innerHTML = `I'm have a poblem`
-        break;
-    }
-
-    const uptime = stat.all_time_uptime_ratio
-    
-    labels.push(`uptime ${Math.round(uptime)}%`)
-    labels.push(`downtime ${100 - (Math.round(uptime))}%`)
-    
-    data.push(uptime) 
-    data.push(100 - uptime)
-    
-    backgroundColor.push('white')
-    backgroundColor.push('gray')
-    
-
-    let getChart = new Chart(billyChart, {
-      type: 'pie',
-      data: {
-        labels,
-        datasets: [{
-          //label: '# milisec',
-          data,
-          backgroundColor,
-          borderColor: 'white',
-          borderWidth: 1
-        }]
+        default:
+          statusWeb.innerHTML = `I'm have a poblem`;
+          break;
       }
 
-    })
-    
-    if (stat.status !== 2) return
+      let uptime = Number(
+        `${stat.all_time_uptime_ratio}`
+          .split(".")
+          .map((p, i) => (i === 1 ? (p = `${p.charAt(0)}${p.charAt(1)}`) : p))
+          .join(".")
+      );
 
-    await fetch('https://billy-bot-1.yansaan.repl.co').then(r => r.json()).then(r => {
-    let statusTable = ''
-    r.detail.forEach(d => {
-      statusTable += `<tr>
-      <td>${d.name}</td>
+      let downtime = Number(
+        `${100 - stat.all_time_uptime_ratio}`
+          .split(".")
+          .map((p, i) => (i === 1 ? (p = `${p.charAt(0)}${p.charAt(1)}`) : p))
+          .join(".")
+      );
+
+      labels.push(`uptime ${uptime}%`);
+      labels.push(`downtime ${downtime}%`);
+
+      data.push(uptime);
+      data.push(downtime);
+
+      backgroundColor.push("white");
+      backgroundColor.push("gray");
+
+      let getChart = new Chart(billyChart, {
+        type: "pie",
+        data: {
+          labels,
+          datasets: [
+            {
+              //label: '# milisec',
+              data,
+              backgroundColor,
+              borderColor: "white",
+              borderWidth: 1,
+            },
+          ],
+        },
+      });
+
+      if (stat.status !== 2) {
+        tableStat.innerHTML = `<h3 class="text-white">bot is disabled!!!</h3>`;
+        return;
+      }
+
+      await fetch("https://billy-bot-1.yansaan.repl.co")
+        .then(r => r.json())
+        .then(r => {
+          let statusTable = "";
+          r.detail.forEach(d => {
+            statusTable += `<tr>
+      <td><a id="detail" href="#name-function" data-desc="${
+        d.description
+      }" data-type="${d.type}" data-enable="${
+              d.enable ? "enabled" : "disabled"
+            }">${d.name}</a></td>
       <td>${d.type}</td>
-      <td class="">${(d.enable)?'✅':'❌'}</td>
-      </tr>`
-    })
+      <td class="">${d.enable ? "✅" : "❌"}</td>
+      </tr>`;
+          });
 
-    tableStat.innerHTML = `<table class="table table-dark table-borderless">
+          tableStat.innerHTML = `<table class="table table-dark table-borderless">
     <thead>
     <tr>
     <th scope="col">Name</th>
@@ -77,9 +102,26 @@ window.onload = async function() {
     <tbody>
     ${statusTable}
     </tbody>
-    </table>`
-  })
+    </table>`;
+        });
+    });
+};
 
-  })
+document.addEventListener("click", e => {
+  if (e.target.id === "detail") {
+    let fcnName = document.getElementById("name-function");
+    let fcnStatus = document.getElementById("status-function");
+    let fcnDesc = document.getElementById("desc-function");
 
-}
+    // e.preventDefault();
+    fcnName.innerHTML = e.target.innerHTML;
+    fcnStatus.innerHTML = `Type: ${e.target.dataset.type} | Status: ${e.target.dataset.enable}`;
+    fcnDesc.innerHTML = e.target.dataset.desc;
+    cardDetail.classList.remove("d-none");
+  }
+});
+
+closeCard.addEventListener("click", e => {
+  e.preventDefault();
+  cardDetail.classList.add("d-none");
+});
